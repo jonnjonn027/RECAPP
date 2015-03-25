8d0d5a91dcb5dc1ec134699b2821e4aff7e6c4d8
jonnjonn027 <jon.a.singer@gmail.com>
2015-03-25 16:45:41 -0700

    Create popup2.js

---
+++ popup2.js
@@ -0,0 +1,8 @@
+window.addEventListener('load',function(){
+	document.getElementById('submitbutton').addEventListener("click", function() {
+		var firstName = document.getElementById('firstName')
+		var middleName = document.getElementById('middleName')
+		var lastName = document.getElementById('lastName')
+		var domain = document.getElementById('domain') 
+	} );
+} );
